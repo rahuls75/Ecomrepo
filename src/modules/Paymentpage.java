b14bf34546fe7680619f3165a6ce88f02f6d06ca
rahuls75 <161546561+rahuls75@users.noreply.github.com>
2024-03-07 18:16:25 +0530

    main branch commit

---
+++ src/modules/Paymentpage.java
@@ -0,0 +1,12 @@
+package modules;
+
+public class Paymentpage {
+	
+	public static void main(String[] args) {
+
+		System.out.println("user is at payment page");
+		System.out.println("user added the the information for payment"); 
+		System.out.println("user clicked on the pay now option");
+
+}
+}
---
+++ src/modules/Productpage.java
@@ -0,0 +1,10 @@
+package modules;
+
+public class Productpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
